e6bed876008d482f10ba241d7a83eb8bcf357b0d
engammar111 <34649325+engammar111@users.noreply.github.com>
2020-04-19 17:41:28 +0200

    intia; commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className='display-4'>About Contact Manager</h1>
+      <p className='lead'>Simple app to manage contacts</p>
+      <p className='text-secondary'>
+        Technology Used{' '}
+        <span className='text-danger'>
+          REACT,REDUX,REDUX-THUNK,BOOTSTRAP,API
+        </span>
+      </p>
+      <p className='text-secondary'>Version 1.0.0</p>
+    </div>
+  );
+};
